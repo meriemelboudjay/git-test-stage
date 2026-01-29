bbb27e8c74094cb8c50898ca887bc2b9e0df6afe
Meriem El Boudjay <meriem.elboudjay.23@ump.ac.ma>
2026-01-29 21:31:34 +0100

    Ajout des fichiers Java de base

---
+++ src/User.java
@@ -0,0 +1,15 @@
+public class User {
+
+    private String name;
+    private String role;
+
+    public User(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Role: " + role);
+    }
+}
